df2652ac84b890cd7972eba9242473f7c636e766
Erik Härkönen <erik.harkonen@relexsolutions.com>
2016-06-17 00:43:20 +0300

    Removed unused code from kernelreader

--- src/kernelreader.cpp
+++ src/kernelreader.cpp
@@ -7,11 +7,5 @@ void kernelFromFile(const std::string filename, cl::Context &context, cl::Progra
     buffer << f.rdbuf();
     
     const std::string &tmp = buffer.str();
-    //const char *cstr = tmp.c_str();
-    //size_t programSize = tmp.size();
-
-    //cl::Program::Sources cl_source(1, std::make_pair(cstr, programSize + 1));
-    //cl::Program::Sources cl_source;
-    //cl_source.push_back(tmp);
     program = cl::Program(context, tmp, false, &err);
 }
