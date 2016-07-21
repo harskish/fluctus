2d1d723f35b53bcc246546dd309b928aa2b6fdae
Erik Härkönen <erik.harkonen@hotmail.com>
2016-07-21 23:15:58 +0300

    Added ply loader, teapot model

--- src/scene.cpp
+++ src/scene.cpp
@@ -98,11 +98,131 @@ void Scene::loadObjModel(const std::string filename)
     unpackIndexedData(positions, normals, faces, false);
 }
 
+/* Used for loading PLY meshes */
 void Scene::loadPlyModel(const std::string filename)
 {
-    (void)filename;
-    std::cout << "Not implemented!" << std::endl;
-    exit(1);
+    struct Element
+    {
+        std::string name;			     // e.g. vertex
+        int lines;				         // e.g. 1300
+        std::vector<std::string> props;  // e.g. [x, y, z, nx, ny, nz]
+    };
+
+    // Keep track of what data, and how meny elements each, to expect
+    std::vector<Element> elements;
+
+    // Open input file stream for reading.
+    std::ifstream input(filename, std::ios::in);
+
+    std::string line;
+
+    // Data of current element
+    std::string type = "none";
+    int num_elem = 0;
+    std::vector<std::string> currentProps;
+
+    /* READ HEADERS */
+    while (getline(input, line))
+    {
+        std::istringstream iss(line);
+        std::string s;
+        iss >> s;
+        if (s == "element")
+        {
+            elements.push_back(Element{ type, num_elem, currentProps }); //Push previous element
+            currentProps.clear();
+            iss >> type >> num_elem;
+        }
+        else if (s == "property")
+        {
+            std::string type, name;
+            iss >> type >> name;
+            currentProps.push_back(name);
+        }
+        else if (s == "end_header")
+        {
+            elements.push_back(Element{ type, num_elem, currentProps }); //push last element
+            break;
+        }
+    }
+
+    std::cout << "PLY headers processed" << std::endl;
+
+    std::vector<float3> positions, normals;
+    std::vector<std::array<unsigned, 6>> faces;
+
+    /* READ DATA */
+    for (Element &e : elements)
+    {
+        if (e.name == "vertex")
+        {
+            std::cout << "Reading " << e.lines << " vertices" << std::endl;
+            for (int i = 0; i < e.lines; i++)
+            {
+                getline(input, line);
+                std::istringstream iss(line);
+
+                std::map<std::string, float> map;
+                float bucket;
+                for (std::string name : e.props)
+                {
+                    iss >> bucket;
+                    map[name] = bucket;
+                }
+
+                positions.push_back(float3(map["x"], map["y"], map["z"]));
+                if (map.find("nx") != map.end()) //contains normals
+                    normals.push_back(float3(map["nx"], map["ny"], map["nz"]));
+            }
+        }
+        else if (e.name == "face")
+        {
+            std::cout << "Reading " << e.lines << " faces" << std::endl;
+            for (int i = 0; i < e.lines; i++)
+            {
+                getline(input, line);
+                std::istringstream iss(line);
+
+                std::array<unsigned, 6>  f; // Face index array
+
+                // Face list format: '3 i1 i2 i3' (triangle) or '4 i1 i2 i3 i4' (quad)
+                // A quad can be represented with two triangles
+
+                int poly_type;
+                iss >> poly_type;
+
+                if (poly_type == 3)
+                {
+                    iss >> f[0] >> f[2] >> f[4];
+                    faces.push_back(f);
+                }
+                else if (poly_type == 4)
+                {
+                    int i0, i1, i2, i3;
+                    iss >> i0 >> i1 >> i2 >> i3;
+
+                    //triangle 1
+                    f[0] = i0; f[2] = i1; f[4] = i2;
+                    faces.push_back(f);
+
+                    //triangle 2
+                    f[0] = i2; f[2] = i3; f[4] = i0;
+                    faces.push_back(f);
+                }
+            }
+        }
+        else
+        {
+            //skip data
+            std::cout << "Skipping element of type " << e.name << std::endl;
+            for (int i = 0; i < e.lines; i++)
+            {
+                getline(input, line);
+            }
+        }
+    }
+
+    unpackIndexedData(positions, normals, faces, true); //true = ply format
 }
 
 void Scene::unpackIndexedData(const std::vector<float3> &positions,
--- src/scene.hpp
+++ src/scene.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <vector>
+#include <map>
 #include <array>
 #include <iostream>
 #include <fstream>
