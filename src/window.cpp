76f67012eec68daa0e4e3958c809cc5ac7cc324a
Erik Härkönen <erik.harkonen@hotmail.com>
2016-09-18 18:13:01 +0300

    Use 32bit textures

--- src/window.cpp
+++ src/window.cpp
@@ -155,7 +155,7 @@ void PTWindow::createTextures()
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_FLOAT, 0);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, width, height, 0, GL_RGBA, GL_FLOAT, 0);
     }
     
     glBindTexture(GL_TEXTURE_2D, 0);
